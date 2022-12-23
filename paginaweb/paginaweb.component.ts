1dbc011a32d833de514257ddba88020f11789105
jefry145 <116960895+jefry145@users.noreply.github.com>
2022-12-23 15:49:48 -0500

    Add files via upload

---
+++ paginaweb/paginaweb.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paginaweb',
+  templateUrl: './paginaweb.component.html',
+  styleUrls: ['./paginaweb.component.css']
+})
+export class PaginawebComponent {
+
+}
